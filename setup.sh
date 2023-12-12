30ff09b94f9a054a4085a06098b7bcb10f75074f
Rashad Aliyev <rasad2k@gmail.com>
2023-12-12 20:03:04 +0100

    Added formatting and zsh configuration
    - Added powerlevel10k configuration
    - Added .zshrc
    - Updated setup script to configure zsh
    - Formatted setup.sh

--- setup.sh
+++ setup.sh
@@ -1,16 +1,13 @@
-function install_package  {
-	if command -v pacman &> /dev/null
-	then
+function install_package {
+	if command -v pacman &>/dev/null; then
 		sudo pacman -S $1
 		return 0
 	fi
-	if command -v apt &> /dev/null
-	then
+	if command -v apt &>/dev/null; then
 		sudo apt install $1
 		return 0
 	fi
-	if command -v dnf &> /dev/null
-	then
+	if command -v dnf &>/dev/null; then
 		sudo dnf install $1
 		return 0
 	fi
@@ -18,8 +15,7 @@ function install_package  {
 }
 
 function check_command {
-	if ! command -v $1 &> /dev/null
-	then
+	if ! command -v $1 &>/dev/null; then
 		echo "[-] '$1' not found!"
 		echo "[+] Installing '$1'."
 		install_package $1
@@ -32,12 +28,16 @@ function install_prerequirements {
 	check_command git
 	check_command curl
 	check_command zsh
-    check_command alacritty
+	check_command alacritty
 	check_command tmux
 }
 
 function install_omz {
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --skip-chsh --unattended
+}
+
+function install_powerlevel10k {
+	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 }
 
 function install_fonts {
@@ -60,8 +60,8 @@ function configure_zsh {
 	install_powerlevel10k
 	git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-	cp ./.zshrc /home/$USER/.zshrc
-	cp ./.p10k.zsh /home/$USER/.p10k.zsh
+	cp ./.zshrc ~/.zshrc
+	cp ./.p10k.zsh ~/.p10k.zsh
 }
 
 function configure_picom {
@@ -70,10 +70,10 @@ function configure_picom {
 
 function main {
 	install_prerequirements
-	install_omz
 	install_fonts
-    configure_tmux
-    configure_alacritty
+	configure_tmux
+	configure_alacritty
+	configure_zsh
 }
 
 main
