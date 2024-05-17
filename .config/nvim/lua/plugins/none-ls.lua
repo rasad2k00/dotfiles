b544e5d9180297dcb4d6ef04901a49a950991623
Rashad Aliyev <rasad2k@gmail.com>
2024-05-17 21:37:25 +0200

    Updated neovim config

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -5,7 +5,7 @@ return {
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.gofmt,
+				null_ls.builtins.formatting.goimports,
 				null_ls.builtins.formatting.black,
 				null_ls.builtins.formatting.isort,
 				null_ls.builtins.formatting.shfmt,
--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -1,11 +1,11 @@
-vim.opt.guicursor = ""
-
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 
 vim.opt.relativenumber = true
 vim.opt.number = true
+
+vim.opt.termguicolors = true
 
 vim.g.mapleader = " "
 
