18999bd35c36aefc1f36013b43ae28b649c81b21
Rashad Aliyev <rasad2k@gmail.com>
2024-06-11 18:00:06 +0200

    Updated neovim config

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -8,6 +8,6 @@ return {
 		"3rd/image.nvim",        -- Optional image support in preview window: See `# Preview Mode` for more information
 	},
 	config = function()
-		vim.keymap.set("n", "<leader>n", ":Neotree float<CR>")
+		vim.keymap.set("n", "<leader>n", ":Neotree right<CR>")
 	end,
 }
