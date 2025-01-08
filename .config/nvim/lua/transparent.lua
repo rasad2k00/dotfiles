350aa44d7b11ef04682f563c652f46849549fb66
Rashad Aliyev <rasad2k@gmail.com>
2025-01-08 23:01:10 +0100

    Updated neovim config

--- .config/nvim/lua/transparent.lua
+++ .config/nvim/lua/transparent.lua
@@ -2,20 +2,20 @@
 vim.api.nvim_create_autocmd("ColorScheme", {
 	callback = function()
 		local highlights = {
-			-- "Normal",
-			-- "LineNr",
-			-- "Folded",
-			-- "NonText",
-			-- "SpecialKey",
-			-- "VertSplit",
-			-- "SignColumn",
-			-- "EndOfBuffer",
-			-- "NeoTreeNormal",
-			-- "NeoTreeNormalNC",
-			-- "NormalNc",
-			-- "TablineFill", -- this is specific to how I like my tabline to look like
-			-- "NormalFloat",
-			-- "NormalFloatBorder",
+			"Normal",
+			"LineNr",
+			"Folded",
+			"NonText",
+			"SpecialKey",
+			"VertSplit",
+			"SignColumn",
+			"EndOfBuffer",
+			"NeoTreeNormal",
+			"NeoTreeNormalNC",
+			"NormalNc",
+			"TablineFill", -- this is specific to how I like my tabline to look like
+			"NormalFloat",
+			"NormalFloatBorder",
 		}
 		for _, name in pairs(highlights) do
 			vim.cmd.highlight(name .. " guibg=none ctermbg=none")
