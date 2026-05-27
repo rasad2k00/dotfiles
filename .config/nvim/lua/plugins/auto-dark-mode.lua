cbab362f9546a1be3c0e4ecb7ae97286b88ee173
Rashad Aliyev <rasad2k@gmail.com>
2026-05-27 03:24:49 +0200

    Added auto-dark-mode plugin

---
+++ .config/nvim/lua/plugins/auto-dark-mode.lua
@@ -0,0 +1,18 @@
+-- Lua
+
+return {
+	"f-person/auto-dark-mode.nvim",
+	priority = 1000,
+	opts = {
+		update_interval = 3000,
+		set_dark_mode = function()
+			vim.api.nvim_set_option_value("background", "dark", {})
+			vim.cmd("colorscheme catppuccin")
+		end,
+		set_light_mode = function()
+			vim.api.nvim_set_option_value("background", "light", {})
+			vim.cmd("colorscheme zellner")
+		end,
+		fallback = "dark",
+	},
+}
